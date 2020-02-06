53f968e5999fc7437d0150613a8afa95315c7a78
sonnguyen89 <nam_son14@yahoo.com>
2020-02-06 22:17:59 +0700

    update style

--- wp-content/themes/wp-bootstrap-4/template-parts/front-page/contract.php
+++ wp-content/themes/wp-bootstrap-4/template-parts/front-page/contract.php
@@ -7,8 +7,8 @@
             <div class="col">
                 <div class="social-links float-left">
                     <ul>
-                        <li><a href="tel:+64488125904"><span><i class="fa fa-phone"></i>  0488 125 904</span></a></li>
-                        <li><a href="#"><span><i class="fa fa-map-marker"></i>   Greenvale, Victoria</span></a></li>
+                        <li><a href="tel:+64488125904"><span><i class="fa fa-phone" style="margin-left: 2px;margin-right: 28px;"></i>  0488 125 904</span></a></li>
+                        <li><a href="#"><span><i class="fa fa-map-marker" style="margin-left: 5px;"></i>   Greenvale, Victoria</span></a></li>
                         <li><a href="mailto:beautyboutiquebyelyse@outlook.com"><span><i class="fa fa-envelope"></i>beautyboutiquebyelyse@outlook.com</span></a></li>
                     </ul>
                 </div>
