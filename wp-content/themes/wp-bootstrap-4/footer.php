37ad37529e78f057911d33206023776e7c3c157e
sonnguyen89 <nam_son14@yahoo.com>
2020-02-07 17:24:02 +0700

    update style

--- wp-content/themes/wp-bootstrap-4/footer.php
+++ wp-content/themes/wp-bootstrap-4/footer.php
@@ -17,12 +17,24 @@
 
 		<div class="container">
             <div class="row">
-                <div class="col-sm">
+                <div class="col-sm-12">
+                    <div class="mobile-social-links">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fa fa-envelope"></i></a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
                     <div class="site-info">
                         © 2020 Beauty Boutique By Elyse. Website & Digital Marketing By  <a href="<?php echo esc_url( 'https://www.uberink.com.au/' ); ?>"><?php esc_html_e( 'UBERINK', 'wp-bootstrap-4' ); ?></a>
                     </div><!-- .site-info -->
                 </div>
-                <div class="col-sm">
+                <div class="col-sm-6">
                     <div class="social-links">
                         <ul>
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
--- wp-content/themes/wp-bootstrap-4/template-parts/front-page/contract.php
+++ wp-content/themes/wp-bootstrap-4/template-parts/front-page/contract.php
@@ -1,18 +1,18 @@
-<section class="wp-bp-contract-content">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <img src="/wp-content/themes/wp-bootstrap-4/assets/images/beauty_boutique_by_elyse_logo.jpg" class="float-right"/>
-            </div>
-            <div class="col">
-                <div class="social-links float-left">
-                    <ul>
-                        <li><a href="tel:+64488125904"><span><i class="fa fa-phone" style="margin-left: 2px;margin-right: 28px;"></i>  0488 125 904</span></a></li>
-                        <li><a href="#"><span><i class="fa fa-map-marker" style="margin-left: 5px;"></i>   Greenvale, Victoria</span></a></li>
-                        <li><a href="mailto:beautyboutiquebyelyse@outlook.com"><span><i class="fa fa-envelope"></i>beautyboutiquebyelyse@outlook.com</span></a></li>
-                    </ul>
-                </div>
+<div class="wp-bp-contract-content container">
+
+    <div class="row">
+        <div class="col-sm-6">
+            <img src="/wp-content/themes/wp-bootstrap-4/assets/images/beauty_boutique_by_elyse_logo.jpg" class="float-right"/>
+        </div>
+        <div class="col-sm-6">
+            <div class="social-links">
+                <ul>
+                    <li><a href="tel:+64488125904"><span><i class="fa fa-phone" style="margin-left: 2px"></i>  0488 125 904</span></a></li>
+                    <li><a href="#"><span><i class="fa fa-map-marker" style="margin-left: 5px;"></i>   Greenvale, Victoria</span></a></li>
+                    <li><a href="mailto:beautyboutiquebyelyse@outlook.com"><span><i class="fa fa-envelope"></i>beautyboutiquebyelyse@outlook.com</span></a></li>
+                </ul>
             </div>
         </div>
     </div>
-</section>
+
+</div>
