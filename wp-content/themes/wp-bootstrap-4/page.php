1d9750f72156c52a1914faf7e3b857df1454f56a
sonnguyen89 <nam_son14@yahoo.com>
2020-02-08 13:57:09 +0700

    update page

---
+++ wp-content/themes/wp-bootstrap-4/page.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WP_Bootstrap_4
+ */
+
+
+if ( 'posts' == get_option( 'show_on_front' ) ) {
+    include( get_home_template() );
+}
+else {
+    if ( ! is_page_template() ) {
+        get_header();
+        ?>
+        <section class="wp-bp-main-content">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <?php while ( have_posts() ) : the_post(); ?>
+                            <h2 class="text-center mb-4"><?php the_title(); ?></h2>
+                            <?php wp_bootstrap_4_post_thumbnail(); ?>
+                            <?php the_content(); ?>
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php
+        get_footer();
+    }
+    else {
+        include( get_page_template() );
+    }
+}
+?>
