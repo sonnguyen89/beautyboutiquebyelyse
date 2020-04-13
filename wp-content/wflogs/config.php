688e6e920f1179b2f355cbafd1875d9d734dd09b
sonnguyen89 <nam_son14@yahoo.com>
2020-04-13 15:49:06 +1000

    remove wflogs

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1583239759;s:7:"authKey";s:64:"H< A E:XkD4$B*OHK/kr{36$ofi3,$0*4z(qtqr3BEc?kz0}u&]F?fU7<YHs7V^k";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:4095;}
